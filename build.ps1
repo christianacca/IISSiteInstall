7ffeedf2b6fc7cc5a155ceb5dad66667fe31facf
Christian Crowhurst <ccrowhurst@ramplc.com>
2017-12-02 14:33:33 +0000

    chore(CI): getting ready for CI on appveyor

--- build.ps1
+++ build.ps1
@@ -8,12 +8,31 @@ Derived from scripts written by Warren F. (RamblingCookieMonster)
 param ($Task = 'Default')
 Write-Output "Starting build"
 
+# Register repo (used to publish)
+$publishRepository = 'christianacca-ps'
+Set-Item Env:\PublishRepo -Value $publishRepository
+if (-not(Get-PSRepository -Name $publishRepository -EA SilentlyContinue))
+{
+    Write-Output "  Registering custom PS Repository '$publishRepository'"
+    Import-Module PowerShellGet
+    
+    $repo = @{
+        Name                  = $publishRepository
+        SourceLocation        = 'https://www.myget.org/F/christianacca-ps/api/v2'
+        ScriptSourceLocation  = 'https://www.myget.org/F/christianacca-ps/api/v2/'
+        PublishLocation       = 'https://www.myget.org/F/christianacca-ps/api/v2/package'
+        ScriptPublishLocation = 'https://www.myget.org/F/christianacca-ps/api/v2/package/'
+        InstallationPolicy    = 'Trusted'
+    }
+    Register-PSRepository @repo
+}
+
 # Grab nuget bits, install modules, set build variables, start build.
 Write-Output "  Install And Import Dependent Modules"
 Write-Output "    Build Modules"
 if (-not(Get-InstalledModule PSDepend -RequiredVersion 0.1.56.3 -EA SilentlyContinue))
 {
-    Install-Module PSDepend -RequiredVersion 0.1.56.3 -Repository christianacca-ps
+    Install-Module PSDepend -RequiredVersion 0.1.56.3 -Repository $publishRepository
 }
 Invoke-PSDepend -Path "$PSScriptRoot\build.depend.psd1" -Install -Import -Force
 
--- deploy.PSDeploy.ps1
+++ deploy.PSDeploy.ps1
@@ -20,7 +20,7 @@ if ($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
     Deploy Module {
         By PSGalleryModule {
             FromSource output\$ENV:BHProjectName
-            To PSGallery
+            To $ENV:PublishRepo
             WithOptions @{
                 ApiKey = $ENV:NugetApiKey
             }
