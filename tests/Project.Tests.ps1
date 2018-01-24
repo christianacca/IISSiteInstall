867a4842bf7ce941aba9eb9622e0be568f6dab7e
christianacca <christian.crowhurst@gmail.com>
2018-01-24 22:56:05 +0000

    tests(*): increase test coverage

--- tests/Project.Tests.ps1
+++ tests/Project.Tests.ps1
@@ -5,6 +5,27 @@ $moduleRoot = Split-Path $modulePath
 $moduleName = $env:BHProjectName
 $moduleRoot = $env:BHModulePath
 
+Describe "PSScriptAnalyzer rule-sets" -Tag Build {
+
+    $rulesToExclude = @('PSUseToExportFieldsInManifest', 'PSShouldProcess')
+    $Rules = Get-ScriptAnalyzerRule | where RuleName -NotIn $rulesToExclude
+    $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse | where fullname -notmatch 'classes'
+
+    foreach ( $Script in $scripts ) 
+    {
+        Context "Script '$($script.FullName)'" {
+
+            foreach ( $rule in $rules )
+            {
+                It "Rule [$rule]" {
+
+                    (Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
+                }
+            }
+        }
+    }
+}
+
 Describe "General project validation: $moduleName"  -Tag Build, Unit {
 
     $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
